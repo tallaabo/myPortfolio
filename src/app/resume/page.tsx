9e1cf324e69733e4a200542171f05f932500552d
Aman Has <atallaabo@gmail.com>
2025-09-14 16:04:17 -0500

    renamed to Resume with capital R

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,12 +5,12 @@ export default function ResumePage() {
     <div className="mx-auto max-w-4xl p-6">
       <h1 className="text-2xl font-semibold text-white">Resume</h1>
       <p className="mt-2">
-        <a className="underline text-yellow-300" href="/resume.pdf" download>
+        <a className="underline text-yellow-300" href="/Resume.pdf" download>
           Download PDF
         </a>
       </p>
       <object
-        data="/resume.pdf"
+        data="/Resume.pdf"
         type="application/pdf"
         className="mt-4 h-[80vh] w-full rounded-xl border border-white/30 bg-black"
       />
